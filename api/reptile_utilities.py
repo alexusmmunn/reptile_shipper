0368c1dd81c8c574527f8aeefebf9dd2745a0d69
alexusmmunn <alexusmunn@gmail.com>
2024-12-09 09:02:09 -0800

    Add geonames for future functionality
    Right now we only return the location of one city, eventually
    this function will handle multiple cities lat lon

--- api/reptile_utilities.py
+++ api/reptile_utilities.py
@@ -2,11 +2,23 @@
 
 from dotenv import load_dotenv
 import os
+# https://www.geonames.org/servlet/geonames?
+# requires free account activated here
+from geopy import geocoders
 
 enviornment = os.getenv("ENVIRONMENT")
+geonames_username = os.getenv("GEONAMES_USERNAME")
+geo = geocoders.GeoNames(username=geonames_username)
 
 def is_local():
     if enviornment == "development":
         return True
     else:
         return False
+    
+def get_lat_lon(city):
+    # TODO: Modify so this takes any name of a city and returns lat lon
+    # Right now geonames isn't working... I'm getting 401s
+    if city == 'sacramento':
+        return (38.5781,121.4944)
+    return None
