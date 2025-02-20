83186d234b3e0c2bb4e5dfc216ff9a91b2501789
alexusmmunn <alexusmunn@gmail.com>
2025-02-19 20:38:28 -0800

    Add rate limiting

--- api/app.py
+++ api/app.py
@@ -1,13 +1,30 @@
 from flask import Flask, jsonify, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import os
 
 from seven_day_weather import city_seven_day_forcast
+from reptile_utilities import is_local
 from error_codes import HTTPError
 
 API_KEY = os.getenv("AUTHENTICATION_KEY")
 
+
+
 # Create a Flask application instance
 app = Flask(__name__)
+
+# Handle rate limiting
+if is_local():
+    RATE_LIMIT = "5 per minute"
+else:
+    RATE_LIMIT = "100 per hour"
+# Initialize Limiter
+limiter = Limiter(
+    get_remote_address,  # Uses client's IP for rate limiting
+    app=app,
+    default_limits=[RATE_LIMIT]
+)
 
 # Authenticator for requests
 def authenticate(request):
